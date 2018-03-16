185b44a6ca5c2043aa2203a179491c03840ea801
kenan23@gmail.com <kenan23@gmail.com>
2018-03-17 01:04:33 +0300

    Reading Coordinates from NCN file

--- ReadCoordinates.py
+++ ReadCoordinates.py
@@ -19,8 +19,6 @@ CADFileLocation = ScriptRunFolder + u'/Input/' + 'kk.DWG'
 ProjectionFile = ScriptRunFolder + u'/Projection/8111.prj'
 CrossSectionIdentifier = u"PL_TOTFKM"
 NCNFileLocation = ScriptRunFolder + u'/Input/kk.NCN'
-
-
 StreamIdentifier = "2"
 ## TODO  NCN file read
 NameOftheCrossSection = "KesikKopru"
